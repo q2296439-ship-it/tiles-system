b504fe335d83c2eb2c6c01ce054ff90995e79160
Nicole Center <q2296439@gmail.com>
2026-04-15 08:48:07 +0800

    colelction modal

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -15,6 +15,12 @@ class Collection extends Model
         'customer_name',
         'address',
         'terms',
+
+        'gross_amount',
+        'discount_type',
+        'discount_amount',
+        'net_amount',
+
         'total_amount',
         'branch_id',
         'user_id',
@@ -24,6 +30,9 @@ class Collection extends Model
 
     protected $attributes = [
         'status' => 'saved',
+        'gross_amount' => 0,
+        'discount_amount' => 0,
+        'net_amount' => 0,
     ];
 
     public function items()
